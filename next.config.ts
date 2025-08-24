308a012434d05e2da99c152fc01ecb46ba399103
Geeta Pawar <geetamane2010@gmail.com>
2025-08-24 10:51:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.istockphoto.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
